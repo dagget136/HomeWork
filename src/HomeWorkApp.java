f9ad123b4939927daa4378ee940a88f0bef6c670
VladimirToryanik <degget95@gmail.com>
2021-09-12 12:50:15 +0300

    homework lesson 1(fix indentation)

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -6,32 +6,32 @@
  */
 public class HomeWorkApp {
     public static void main(String[] args) {
-    	System.out.println(FirstApp(5, 5));
-    	SecondApp(0);
-    	System.out.println(ThirdApp(-3));
-    	FourthApp(args);
+       System.out.println(FirstApp(5, 5));
+       SecondApp(0);
+       System.out.println(ThirdApp(-3));
+       FourthApp(args);
     }
 
     static boolean FirstApp(int a, int b) {
-		return (a + b <= 20 && a + b >= 10); // { мой первый(удачный) и более громоздкий вариант решения
- //		if (a + b <= 20 && a + b >=10)
- //         return true;
- //     }else {
- //		    return false;
- //     }
-	}
+       return (a + b <= 20 && a + b >= 10); // { мой первый(удачный) и более громоздкий вариант решения
+    //		if (a + b <= 20 && a + b >=10)
+    //         return true;
+    //     }else {
+    //		    return false;
+    //     }
+    }
 
     static void SecondApp(int a) {
-    	System.out.println(a >= 0? "Положительное":"отрицательное");
-	}
+        System.out.println(a >= 0 ? "Положительное" : "отрицательное");
+    }
 
-	static boolean ThirdApp(int a) {
-    	return (a <= 0);
-	}
+    static boolean ThirdApp(int a) {
+        return (a <= 0);
+    }
 
-	static void FourthApp( String[] ar) {
-        for (int a = 0; a < 5; a++  ) {
-        	System.out.println("Строка");
+    static void FourthApp(String[] ar) {
+        for (int a = 0; a < 5; a++) {
+            System.out.println("Строка");
         }
-	}
+    }
 }
