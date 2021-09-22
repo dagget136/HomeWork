5b4182d76b1f254948033e4455e942fa3f707d99
VladimirToryanik <degget95@gmail.com>
2021-09-22 09:51:13 +0300

    HomeWork FIVE is ready

---
+++ src/Person.java
@@ -0,0 +1,72 @@
+public class Person {
+    private String fullName;
+    private String position;
+    private String email;
+    private int phoneNumber;
+    private int salary;
+    private int age;
+
+    public Person(String fullName, String position, String email, int phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return fullName + ", " + position + ", " + email + ", " + phoneNumber + ", " + salary + ", " + age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
